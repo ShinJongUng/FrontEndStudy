7951c217ce0204f045a845aa9d379355a88ca98a
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2022-01-27 14:47:51 +0900

    Add files via upload

---
+++ Vue_basic/computed cache.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>{{reversedMessage}}</h1>
+  <h1>{{reversedMessage}}</h1>
+  <h1>{{reversedMessage}}</h1>
+  <h1>{{reversedMessage}}</h1>
+  <h1>{{reversedMessage}}</h1>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      msg: 'Hello Computed!'
+    }
+  },
+  computed:{
+      reversedMessage(){
+      return this.msg.split('').reverse().join('')
+    }// computed 안에선 한번만 연산 후 기억 즉 캐싱하고 있음
+  },
+  methods: {
+    reverseMessage(){
+      return this.msg.split('').reverse().join('')
+    }
+  }
+}
+</script>
