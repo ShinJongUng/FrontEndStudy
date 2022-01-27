3595f34eec8cdd846502a60a2d687ee6bb98eeed
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2022-01-27 14:30:11 +0900

    Add files via upload

---
+++ Vue_basic/templateGrammer.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1 
+    v-once
+    @[event]="add">
+      {{ msg }}
+  </h1>
+  <h1 v-html="msg">
+  </h1>
+
+  <h1 :[attr]="amsg">{{amsg}}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      msg: '<div style ="color:red;">Hello!</div>',
+      amsg: 'active',
+      attr: 'class',
+      event: 'click'
+    }
+  },
+  methods:{
+    add(){
+      this.msg += '!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .active{
+    color:royalblue;
+    font-size: 100px;
+  }
+</style>
