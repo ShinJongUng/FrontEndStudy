29bea76f5abe89d2c97f61872f36d209349c2eb1
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2022-01-27 14:15:15 +0900

    Add files via upload

---
+++ Vue_basic/LifeCycle.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>{{ count }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      count:2
+    }
+  },
+  beforeCreate(){
+    console.log('Before Created!', this.count)
+  },
+  created(){
+    console.log('Created!', this.count)
+  },
+  beforeMount(){
+    console.log('Before Mounted!')
+  },
+  mounted(){
+    console.log('Mounted!')
+  
+  }
+}
+</script>
