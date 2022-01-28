2e4dc9b5c23ed4f38c9749c1cd2318e022be16a8
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2022-01-28 18:20:26 +0900

    Add files via upload

---
+++ Vue_basic/form-input-binding.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{msg}}</h1>
+  <input type="text" :value="msg" @input="msg = $event.target.value" />
+  <input type="checkbox" v-model="checked">
+  <h1>{{checked}}</h1>
+</template>
+  
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello world!',
+      checked: false
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
