c8d74717d2a77bc18cb6ff3ac09fc267860f73e4
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2022-01-28 15:05:16 +0900

    Add files via upload

---
+++ Vue_basic/v-for-render.vue
@@ -0,0 +1,41 @@
+<template>
+  <button @click="handler">
+    Click
+  </button>
+  <ul>
+    <li
+    v-for="{id,name} in newFruits"
+    :key="id">
+    {{name}}-{{id}}
+    </li>
+  </ul>
+</template>
+  
+
+<script>
+import shortid from 'shortid'
+export default {
+  data() {
+    return{
+      fruits: ['Apple', 'Banana', 'Cherry']
+    }
+  },
+  computed:{
+    newFruits(){
+      return this.fruits.map(fruit=> {
+        return{
+          id: shortid.generate(),
+          name:fruit
+        }
+      })
+    }
+  },
+  methods:{
+    handler(){
+      this.fruits.push('orange')
+    }
+  }
+ 
+}
+</script>
+
