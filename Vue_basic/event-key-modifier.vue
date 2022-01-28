d9330bf5c7bc7e10b25c56fe55dfb6ee5c2bb925
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2022-01-28 18:06:34 +0900

    Add files via upload

---
+++ Vue_basic/event-key-modifier.vue
@@ -0,0 +1,18 @@
+<template>
+  <input type="text" @keydown.ctrl.a="handler" />
+</template>
+  
+
+<script>
+export default {
+  data() {
+  },
+  computed:{
+  },
+  methods:{
+    handler(event){
+      console.log('Enter')
+    }
+  }
+}
+</script>
