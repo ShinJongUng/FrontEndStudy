d45dc723d28890b168b20fd992ff47d0982dc9d8
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2022-01-28 21:20:31 +0900

    Add files via upload

---
+++ Vue_basic/components-provide,inject/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="message = 'Good?'">
+    Click
+  </button>
+  <h1>App: {{ message }}</h1>
+  <Parent />
+</template>
+
+<script>
+import Parent from '~/components/Parent'
+import {computed} from 'vue'
+
+export default {
+    components:{
+        Parent
+    },
+    data() {
+      return{
+        message: 'Hello world!'
+      }
+    },
+    provide() {
+      return{
+        msg: computed(() =>{
+          return this.message
+        })
+      }
+    }
+}
+</script>
