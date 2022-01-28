134c5b196caaed3ffcf52023076552a22ac49b4d
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2022-01-28 13:38:49 +0900

    Add files via upload

---
+++ Vue_basic/class and stylebinding.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1 
+  v-bind:class="{active: isActive}"
+  :style="{color, fontSize}"
+  @click="activate">
+    Hello?! {{isActive}}
+  </h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      isActive: false,
+      color: 'orange',
+      fontSize: '30px'
+    }
+  },
+  methods: {
+    activate() {
+      this.isActive = true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .active {
+    color: red;
+    font-weight: bold;
+  }
+</style>
