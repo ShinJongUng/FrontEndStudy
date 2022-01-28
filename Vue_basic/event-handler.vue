fce4605a1e9f3f7d1c5ee0b86070a0b780c95754
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2022-01-28 15:22:35 +0900

    Add files via upload

---
+++ Vue_basic/event-handler.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click="handler('hi')">
+    Click 1
+  </button>
+  <button @click="handler('wht')">
+    Click 2
+  </button>
+</template>
+  
+
+<script>
+import shortid from 'shortid'
+export default {
+  data() {
+  },
+  computed:{
+  },
+  methods:{
+    handler(msg){
+      console.log(msg)
+    }
+  }
+ 
+}
+</script>
+
