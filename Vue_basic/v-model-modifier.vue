ce44477ebcdeaae01bd8e027d896850f4f26c749
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2022-01-28 18:46:40 +0900

    Add files via upload

---
+++ Vue_basic/v-model-modifier.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>{{msg}}</h1>
+  <input type="text" v-model.trim="msg">
+</template>
+  
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'hello world'
+    }
+  },
+  methods: {
+  
+  },
+  watch: {
+    msg() {
+      console.log(this.msg)
+    }
+  }
+}
+</script>
