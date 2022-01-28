ee783e3c626f9c231a4d62e8e9299295f61d42ad
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2022-01-28 13:22:36 +0900

    Add files via upload

---
+++ Vue_basic/watch.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1 @click="changeMessage">{{msg}}</h1>
+  <h1>{{reversedMessage}}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      msg: 'hello?'
+    }
+  },
+  computed: {
+    reversedMessage(){
+      return this.msg.split('').reverse().join('')
+    }
+  },
+  watch:{
+    msg(){
+      console.log('msg: ', this.msg)
+    },
+    reversedMessage(){
+      console.log('reversedMessage', this.reversedMessage)
+    }
+  },
+  methods: {
+    changeMessage(){
+      this.msg = 'Good!'
+    }
+  }
+}
+</script>
