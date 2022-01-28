acb26717383d3159c12e281903b96454886f865a
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2022-01-28 14:23:13 +0900

    Add files via upload

---
+++ Vue_basic/v-if-render.vue
@@ -0,0 +1,31 @@
+<template>
+  <button @click="handler">click me</button>
+  <template v-if="isShow">
+    <h1>hello</h1>
+    <p>dd</p>
+    <p>ddd</p>
+  </template>
+  
+
+  <h1 v-else-if="count >3">Count > 3</h1>
+  <h1 v-else>good</h1>
+  
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      isShow: true,
+      count: 0
+    }
+  },
+  methods: {
+    handler() {
+      this.isShow = !this.isShow
+      this.count++
+    }
+  }
+}
+</script>
+
