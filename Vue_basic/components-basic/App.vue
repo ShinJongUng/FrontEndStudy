18b34ad173018ba73abf90f8d244537f20db81d2
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2022-01-28 19:35:12 +0900

    Add files via upload

---
+++ Vue_basic/components-basic/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <MyBtn  color = "red" text="apple"  />
+  <MyBtn  :color = "color"  text="apple" />
+  <MyBtn  color = "royalblue" text="apple" />
+  <MyBtn large text="banana">banana</MyBtn>
+  
+</template>
+  
+
+<script>
+import MyBtn from '~/components/MyBtn'
+export default {
+
+  components:{
+    MyBtn
+  },
+  data() {
+    return{
+      color: '#000'
+    }
+  }
+}
+</script>
