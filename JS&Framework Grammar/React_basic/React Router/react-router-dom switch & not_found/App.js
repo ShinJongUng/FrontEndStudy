5e77d44b08b675c1ca32224484209982e934e1fc
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2022-07-05 22:25:57 +0900

    React study (7/5)

---
+++ JS&Framework Grammar/React_basic/React Router/react-router-dom switch & not_found/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Profile from "./pages/Profile";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/profile/:id" component={Profile} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/about" component={About} />
+        <Route path="/" exact component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
